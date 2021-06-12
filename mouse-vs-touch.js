f5bbbd20ad70d0d5a305b0cdde5413c9ea4545d5
marko-36 <26520303+marko-36@users.noreply.github.com>
2021-06-12 14:11:18 +0200

    Adding simulated click prevention
    Touch event raises a simulated mousemove event, which is captured right after a touch event is caught and 'touch' mode is set, resulting in mode being set back to "mouse' right away. This is prevented by ignoring mousemoves in between a 'touchstart' and a 'click'

--- mouse-vs-touch.js
+++ mouse-vs-touch.js
@@ -1,32 +1,38 @@
 window.inputMode = `undetected`;
 window.mousemovecount = 0;
 window.permanentDetection = true;
+window.preventSimMousemove = false;
 
 removeInputModeListeners = () => {
     document.removeEventListener('touchstart', setInputModeT)
     document.removeEventListener('touchmove', setInputModeT)
     document.removeEventListener('mousemove', addMouseMoves)
     document.removeEventListener('mousedown', () =>{window.mousemovecount = 0})
+    document.removeEventListener('click', () => { window.preventSimMousemove = false })
 }
 
 setInputModeT = () => {
     window.inputMode = 'touch';
+    window.preventSimMousemove = true;    
     document.getElementsByTagName('BODY')[0].className = 'touch'
     console.log('inputMode: touch')
     if (!window.permanentDetection) {removeInputModeListeners()}
 }
 
 addMouseMoves = () => {
-    window.mousemovecount++;
-    if (window.mousemovecount > 1){
-        window.inputMode = 'mouse';
-        document.getElementsByTagName('BODY')[0].className = 'mouse'
-        console.log('inputMode: mouse')
-        if (!window.permanentDetection) {removeInputModeListeners()}                    
-    } 
+    if (!window.preventSimMousemove){
+        window.mousemovecount++;
+        if (window.mousemovecount > 1){
+            window.inputMode = 'mouse';
+            document.getElementsByTagName('BODY')[0].className = 'mouse'
+            console.log('inputMode: mouse')
+            if (!window.permanentDetection) {removeInputModeListeners()}                    
+        }
+    }    
 }
 
 document.addEventListener('touchstart', setInputModeT)
 document.addEventListener('touchmove', setInputModeT)
 document.addEventListener('mousemove', addMouseMoves)
 document.addEventListener('mousedown', () =>{window.mousemovecount = 0})
+document.addEventListener('click', () => { window.preventSimMousemove = false })
